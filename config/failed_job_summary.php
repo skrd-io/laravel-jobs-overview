035aaf097d93a71cabbbd878b6f2341821e80600
Joe S <joe@skrd.io>
2022-02-16 17:11:57 +0000

    Add initial config

---
+++ config/failed_job_summary.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'slack' => [
+        'webhook_url' => env('FAILED_JOB_SUMMARY_SLACK_WEBHOOK_URL')
+    ]
+];
--- src/FailedJobSummaryServiceProvider.php
+++ src/FailedJobSummaryServiceProvider.php
@@ -8,11 +8,19 @@ class FailedJobSummaryServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        //
+        $this->publishConfigs();
     }
 
     public function register(): void
     {
         //
+    }
+
+    protected function publishConfigs(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/failed_job_summary.php',
+            'failed_job_summary'
+        );
     }
 }

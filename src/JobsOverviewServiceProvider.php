7a8896742c1aeffdf8264519a9baa6e8e08fe415
Joe S <joe@skrd.io>
2022-05-11 21:29:48 +0100

    Log job conclusions

--- src/JobsOverviewServiceProvider.php
+++ src/JobsOverviewServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace SkrdIo\JobsOverview;
 
+use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Queue\Events\JobProcessed;
+use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\ServiceProvider;
 use SkrdIo\JobsOverview\Console\NotifyCommand;
+use SkrdIo\JobsOverview\Models\JobConclusion;
 
 class JobsOverviewServiceProvider extends ServiceProvider
 {
@@ -13,6 +17,8 @@ class JobsOverviewServiceProvider extends ServiceProvider
         $this->registerCommands();
 
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        $this->logJobConclusions();
     }
 
     public function register(): void
@@ -33,5 +39,22 @@ class JobsOverviewServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->commands([NotifyCommand::class]);
         }
+    }
+
+    private function logJobConclusions(): void
+    {
+        Queue::after(function (JobProcessed $event) {
+            JobConclusion::create([
+                'type' => $event->job->resolveName(),
+                'is_fail' => false,
+            ]);
+        });
+
+        Queue::failing(function (JobFailed $event) {
+            JobConclusion::create([
+                'type' => $event->job->resolveName(),
+                'is_fail' => true,
+            ]);
+        });
     }
 }

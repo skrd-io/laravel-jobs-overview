295e8203badd9024f5e64b543edec4e369bc3e43
Joe S <joe@skrd.io>
2022-05-12 15:09:55 +0100

    Create JobConclusion factory

---
+++ database/factories/JobConclusionFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use SkrdIo\JobsOverview\Models\JobConclusion;
+
+$factory->define(JobConclusion::class, function (Faker $faker) {
+    return [
+        'type' =>
+            'App\\Jobs\\' .
+            str_replace(' ', '', Str::title($faker->words(3, true))),
+        'is_fail' => $faker->numberBetween(1, 100) <= 20,
+        'concluded_at' => $faker->dateTimeBetween('-1 month', 'now'),
+    ];
+});
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,6 +7,13 @@ use SkrdIo\JobsOverview\JobsOverviewServiceProvider;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withFactories(__DIR__ . '/../database/factories');
+    }
+
     protected function getPackageProviders($app): array
     {
         return [JobsOverviewServiceProvider::class];

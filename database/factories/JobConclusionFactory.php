99cda36c827dd8723d9749b6102bc9fafd875966
Joe S <joe@skrd.io>
2022-05-12 16:34:02 +0100

    Increase success chance to 90%

--- database/factories/JobConclusionFactory.php
+++ database/factories/JobConclusionFactory.php
@@ -9,7 +9,7 @@ $factory->define(JobConclusion::class, function (Faker $faker) {
         'type' =>
             'App\\Jobs\\' .
             str_replace(' ', '', Str::title($faker->words(3, true))),
-        'is_fail' => $faker->numberBetween(1, 100) <= 20,
+        'is_fail' => $faker->numberBetween(1, 100) <= 10,
         'concluded_at' => $faker->dateTimeBetween('-7 days', 'now'),
     ];
 });
